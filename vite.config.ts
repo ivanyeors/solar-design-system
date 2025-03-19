d4bed799d89d08b49e7bc8a5b55a3a30f5199f3b
Ivan Yeo <ivanyeoart@gmail.com>
2025-03-20 00:02:49 +0800

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/solar-design-system/' : '/',
+  base: '/solar-design-system/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
