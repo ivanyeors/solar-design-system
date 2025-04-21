ec00ab26ea41c9da9e41f684218d5a768514fd4e
ivanyeors <ivanyeoart@gmail.com>
2025-04-21 12:05:52 +0800

    added cursor mdc

--- src/styles/semanticTokenPlugin.js
+++ src/styles/semanticTokenPlugin.js
@@ -44,7 +44,7 @@ function categorizeTokens(tokens) {
   return categories;
 }
 
-// Generated on: 2025-03-25T09:02:25.955Z
+// Generated on: 2025-04-10T09:29:56.516Z
 export default plugin(function({ addUtilities }) {
   // Get tokens directory
   const tokensDir = path.join(__dirname, 'tokens/semantic-tokens');
